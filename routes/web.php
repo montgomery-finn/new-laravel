6c730b05f133bade6d0c1be9f91c12596b0c42b4
sandrojr <sjr1999@protonmail.com>
2022-08-30 14:20:09 -0300

    render a blade string

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostsController;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,6 +31,10 @@ Route::get('/endpoint', function () {
 
     // equivalente à:
     // return redirect()->route('home');
+});
+
+Route::get('bladestring', function () {
+    return Blade::render('{{ $greeting }}', ['greeting' => 'Hello']);
 });
 
 //Controller route groups 
